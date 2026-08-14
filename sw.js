7e953ab4ae778209bcf12c6a7179a881f9461e2d
geversonburato-cpu <geversonburato@gmail.com>
2026-08-14 18:36:40 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,13 @@
-const CACHE='video-mixer-v2';const APP=['./','./index.html','./manifest.webmanifest','./icon-192.svg','./icon-512.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP)).then(()=>self.skipWaiting())));self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    self.registration.unregister()
+  );
+});
+
+self.addEventListener('fetch', event => {
+  return;
+});
